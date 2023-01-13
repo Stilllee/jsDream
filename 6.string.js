aa12cb1d11ef48cc183eda270104e3da146eb7e3
Stilllee <orodae@gmail.com>
2023-01-14 02:13:07 +0900

    6.string.js

--- 6.string.js
+++ 6.string.js
@@ -1,2 +1,36 @@
 const textObj = new String('Hello World!');
 const text = 'Hello World!';
+console.log(textObj);
+console.log(text);
+console.log(text.length);
+
+console.log(text[0]);
+console.log(text[1]);
+console.log(text[2]);
+console.log(text.charAt(0));
+console.log(text.charAt(1));
+console.log(text.charAt(2));
+
+console.log(text.indexOf('l'));
+console.log(text.lastIndexOf('l'));
+
+console.log(text.includes('tx'));
+console.log(text.includes('h'));
+console.log(text.includes('H'));
+
+console.log(text.startsWith('He'));
+console.log(text.endsWith('!'));
+
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+
+console.log(text.substring(0, 2));
+console.log(text.slice(2));
+console.log(text.slice(-2));
+
+const space = '           space      ';
+console.log(space.trim());
+
+const longText = 'Got to the, point';
+console.log(longText.split(' '));
+console.log(longText.split(', ', 2));
