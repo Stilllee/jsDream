46e56f49f75a0d44dd347c93bd410ddab6f4fd47
Stilllee <orodae@gmail.com>
2023-01-20 02:32:34 +0900

    1/20

---
+++ 2.add.js
@@ -0,0 +1,21 @@
+const fruits = ['🍎', '🍌', '🍊', '🍇'];
+
+// 배열 아이템을 참조하는 방법
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
+console.log(fruits.length);
+
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+
+//
+// const fruits = ['🍎', '🍌', '🍊', '🍇'];
+// 추가, 삭제 - 좋지 않은 방식 💩
+fruits[fruits.length] = '🍉';
+console.log(fruits);
+
+delete fruits[1];
+console.log(fruits);
